3463b9046bc718e62d0b2f8454d9ab9b210e7a52
Rian K Sinu <riansinu3@gmail.com>
2026-05-04 23:22:07 +0530

    Change default DB password to an empty stringfix

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "Sich@1975",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "ClassroomAllocationSystem",
   waitForConnections: true,
   connectionLimit: 10,
